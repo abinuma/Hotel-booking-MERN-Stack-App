6a8460ff328a034f522b6faa7cbb109b9f7334ed
abinu12 <abinumathewos@gmail.com>
2026-03-07 09:11:30 +0300

    formatted

--- client/src/MyBookings.jsx
+++ client/src/MyBookings.jsx
@@ -27,9 +27,9 @@ const MyBookings = () => {
               <img
                 src={booking.room.images[0]}
                 alt="hotel-image"
-                className="min-md:w-44 rounded shadow object-cover"
+                className="md:w-44 rounded shadow object-cover"
               />
-              <div className="flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4">
+              <div className="flex flex-col gap-1.5 max-md:mt-3 md:ml-4">
                 <p className="font-playfair text-2xl ">
                   {booking.hotel.name}
                   <span className="font-inter text-sm">
@@ -64,13 +64,21 @@ const MyBookings = () => {
               </div>
             </div>
             {/* ------Payment status------ */}
-            <div className="flex flex-col items-start justify-center pt-3" >
-              <div className="flex items-center gap-2" >
-                <div className={`h-3 w-3 rounded-full ${booking.isPaid ? "bg-green-500" : "bg-red-500"}`} ></div>
-                <p className={`text-sm ${booking.isPaid ? "text-green-500" : "text-red-500"}`} >{booking.isPaid ? "Paid" : "Unpaid"}</p>
+            <div className="flex flex-col items-start justify-center pt-3">
+              <div className="flex items-center gap-2">
+                <div
+                  className={`h-3 w-3 rounded-full ${booking.isPaid ? "bg-green-500" : "bg-red-500"}`}
+                ></div>
+                <p
+                  className={`text-sm ${booking.isPaid ? "text-green-500" : "text-red-500"}`}
+                >
+                  {booking.isPaid ? "Paid" : "Unpaid"}
+                </p>
               </div>
               {!booking.isPaid && (
-                <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer" >Pay Now</button>
+                <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">
+                  Pay Now
+                </button>
               )}
             </div>
           </div>
