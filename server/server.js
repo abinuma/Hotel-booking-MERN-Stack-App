a26b20099ee8164a418d7420ccf00f63039837a4
abinu12 <abinumathewos@gmail.com>
2026-03-08 15:56:47 +0300

    implemented clerk-webhook

--- server/server.js
+++ server/server.js
@@ -2,10 +2,20 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
+
 const app = express();
 app.use(cors()); //enable cross-origin resource sharing
+
+//middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+//API to listen to clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
 
